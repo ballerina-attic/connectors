4f3ad76a81db528746ca8d1544c7a5280d4a7b59
dilinisg_89 <dilinisg@gmail.com>
2017-05-29 15:30:39 +0530

    Modify the environment variables

--- linkedin/samples/linkedin/set_env_vars.sh
+++ linkedin/samples/linkedin/set_env_vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ########## LinkedIn Sample ################
-export LINKEDIN_ACCESS_TOKEN="AQVfVsdWhiTBLc2-t-eZueh6SXXhAD-IFrVhtLGgkVV7X71HBfO4oIqCJkGwa7Pgmoo07yVIbxlqynR2mGMisi_Hq5W5FwXZpc8i6DoISzjRpfz4D3t_fy-CKB1oF8yyvtaB_Rvb8GzP_i3Z5Ch6TZTAzfF0pKQl4kc3-EWAev4wyLrAGu0"
+export LINKEDIN_ACCESS_TOKEN="XXXXX"
